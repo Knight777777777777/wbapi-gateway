09b5341b4231f9ea77a96e57ec7c1cf33e93eee9
liancong <lcc1105897090@126.com>
2023-12-18 03:18:02 +0800

    Initial commit

---
+++ src/main/java/com/waterbird/wbapigateway/WbapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.waterbird.wbapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WbapiGatewayApplication.class, args);
+    }
+
+}
